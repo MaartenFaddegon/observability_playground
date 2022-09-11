88d4f55d4e678997bc21f3e751f5f69c02cffbe8
Maarten Faddegon <github@maartenfaddegon.nl>
2022-09-11 14:24:14 +0200

    the react frontend makes a REST call to the nodejs backend and gets a json back

--- todo-frontend/client/src/App.js
+++ todo-frontend/client/src/App.js
@@ -1,19 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 class Clock extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {date: new Date()};
+    this.state = { date: new Date() };
   }
 
   componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
+    this.timerID = setInterval(() => this.tick(), 1000);
   }
 
   componentWillUnmount() {
@@ -22,25 +18,31 @@ class Clock extends React.Component {
 
   tick() {
     this.setState({
-      date: new Date()
+      date: new Date(),
     });
   }
 
   render() {
-    return (
-      <div> The time is {this.state.date.toLocaleTimeString()} </div>
-    );
+    return <div> The time is {this.state.date.toLocaleTimeString()} </div>;
   }
 }
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/todos")
+      .then((res) => res.json())
+      .then((data) => setData(data.todos));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <p> Todo App </p>
         <p>
-          <Clock /> 
-          and you have ?? TODOs
+          <Clock />
+          and you have {!data ? "?loading?" : data} TODOs
         </p>
       </header>
     </div>
--- todo-frontend/server.js
+++ todo-frontend/server.js
@@ -1,16 +1,21 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, 'client/build')));
+app.use(express.static(path.resolve(__dirname, "client/build")));
 
-app.get("/api", (req, res) => {
+app.get("/api", (_req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+app.get("/todos", (_req, res) => {
+  // TODO: make a GRPC here to the todo-backend
+  res.json({ todos: ["boodschappen doen", "koken", "eten", "afwassen"] });
+});
+
+app.get("*", (_req, res) => {
+  res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
 });
 
 app.listen(PORT, () => {
